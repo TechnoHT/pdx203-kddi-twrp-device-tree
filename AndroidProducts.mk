f84cba548ff25d830f2d4ec78357bdceec0fb879
TechnoHT <hieutr134@gmail.com>
2023-03-30 05:22:38 +0900

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_pdx203_kddi.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_pdx203_kddi-user \
+    omni_pdx203_kddi-userdebug \
+    omni_pdx203_kddi-eng
---
+++ omni_pdx203_kddi.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from pdx203_kddi device
+$(call inherit-product, device/sony/pdx203_kddi/device.mk)
+
+PRODUCT_DEVICE := pdx203_kddi
+PRODUCT_NAME := omni_pdx203_kddi
+PRODUCT_BRAND := KDDI
+PRODUCT_MODEL := Pdx203_kddi
+PRODUCT_MANUFACTURER := sony
+
+PRODUCT_GMS_CLIENTID_BASE := android-sony
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="pdx203_kddi-user 11 RKQ1.211001.001 1 dev-keys"
+
+BUILD_FINGERPRINT := KDDI/pdx203_kddi/pdx203_kddi:11/RKQ1.211001.001/1:user/dev-keys
